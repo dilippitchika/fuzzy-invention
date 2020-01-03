d88798c4bd032ed00112e5fd5ed7d388f09e999c
dilippitchika <dilippitchika@gmail.com>
2020-01-03 08:59:44 +0530

    Updated base code

--- src/App.js
+++ src/App.js
@@ -6,18 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <img src="https://imibot.ai/static/assets/img/login/imibotlogo.svg" className="App-logo" alt="logo" />
+        <p
         >
-          Learn React
-        </a>
+          IMImobile's enterprise bot and AI solution
+        </p>
       </header>
     </div>
   );
