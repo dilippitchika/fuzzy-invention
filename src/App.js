6c3e03227a00aa59f170d9524d0daae5fdedf7a1
dilippitchika <dilippitchika@gmail.com>
2020-04-16 12:37:12 +0530

    changes for embeddable preview

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'jsonwebtoken';
+
+function readmeLogin(){
+  const sign = require('jsonwebtoken').sign;
+  const user = {
+    name: 'Dilip',
+    email: 'dilip@piedpiper.com',
+    
+    // User's API Key
+    apiKey: 'yNNzaCNeLB41RmMBY10GFhiba6p0DvM7',
+  };
+
+  const auth_token = sign(user, 'jOWAYRJT1nwbSuRQF4Or');
+  const readmeUrl = 'https://imibot.readme.io/v1.0';
+
+  console.log(`${readmeUrl}?auth_token=${auth_token}`)
+};
 
 function App() {
   return (
@@ -11,6 +28,7 @@ function App() {
         >
           IMImobile's enterprise bot and AI solution
         </p>
+        <button onClick = {readmeLogin}>Print readme url in console</button>
       </header>
     </div>
   );
