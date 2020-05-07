f61e2217876958fca0bd109e2b72eac43821f14e
dilippitchika <dilippitchika@gmail.com>
2020-05-07 14:58:23 +0530

    made readme api call

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'jsonwebtoken';
+import axios from 'axios';
 
 function readmeLogin(){
   const jwt = require('jsonwebtoken');
@@ -14,12 +15,26 @@ const user = {
     // User's API Key
     apiKey: "yNNzaCNeLB41RmMBY10GFhiba6p0DvM7",
 };
-
 const authToken = jwt.sign(user, 'jOWAYRJT1nwbSuRQF4Or');
 const readmeUrl = 'https://docs.imibot.ai/v1.0';
 
 // Redirect your user to this URL to log them in
 console.log(`${readmeUrl}?auth_token=${authToken}`);
+};
+function readmeCORS(){
+  const data = {
+    name: 'Dilip',
+    email: 'dilip@piedpiper.com'}
+
+  const headers = {
+    "content-type":"application/json"
+  }
+  const url = "https://imibotreadmeauth.netlify.app/.netlify/functions/server"
+  axios.post(url,data,{headers:headers})
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    })
 };
 
 function App() {
@@ -32,6 +47,7 @@ function App() {
           IMImobile's enterprise bot and AI solution
         </p>
         <button onClick = {readmeLogin}>Print readme url in console</button>
+        <button onClick = {readmeCORS}>call readme api</button>
       </header>
     </div>
   );
