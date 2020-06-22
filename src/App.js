7a06d77e60a6c43a18fdcf2efb994561686af7b0
dilippitchika <dilippitchika@gmail.com>
2020-06-22 22:59:43 +0530

    dng2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://imibot.ai/static/assets/img/login/imibotlogo.svg" className="App-logo" alt="logo" />
-        <p
-        >
-          IMImobile's enterprise bot and AI solution
-        </p>
+        <img
+          src="https://imibot.ai/static/assets/img/login/imibotlogo.svg"
+          className="App-logo"
+          alt="logo"
+        />
+        <p>IMImobile's enterprise bot and AI solution</p>
       </header>
     </div>
   );
